2296cbf1add8eb0e42d4a80b0b816be7ef8d3fff
Suliman Alsarraf <suliman2@pdx.edu>
2019-10-26 09:38:09 -0700

    Created the setup functions that populates the preprocessed pixel values and labels from the MINST dataset into python lists

--- P2/neuralNetwork.py
+++ P2/neuralNetwork.py
@@ -6,3 +6,71 @@ import numpy as np
 import math
 import random
 import pandas as pd
+
+def setupTrainSet():
+
+	# get the training set path relative to the
+	# execution path
+	TRAINSET = 'mnist_train.csv'
+	dirr = os.path.dirname(__file__)
+	path = os.path.join(dirr, TRAINSET)
+
+	# initialize the lables/output and pixels lists
+	# the lables are meant to be 1D list, and the pixels 2D
+	labels = []
+	pixels = []
+
+	# read in the lables and images info
+	with open(path, 'r') as fil:
+		r = csv.reader(fil)
+		# preprocessing, go through every pixel of every image
+		for i, line in enumerate(r):
+			lbl = (int(line[0]))
+			pxl = line[1:]
+			# insert the bias at the beginning of the list
+			pxl.insert(0, 1.0)
+
+			# divide every pixel value to be between 0 and 1
+			for j in range(len(pxl)):
+				pxl[j] = float(pxl[j])
+				pxl[j] = pxl[j]/255
+
+			# store the values to be returned
+			labels.append(lbl)
+			pixels.append(pxl)
+
+	return labels, pixels
+
+def setupTestSet():
+
+	# get the testing set path relative to the
+	# execution path
+	TESTSET = 'mnist_test.csv'
+	dirr = os.path.dirname(__file__)
+	path = os.path.join(dirr, TESTSET)
+
+	# initialize the lables/output and pixels lists
+	# the lables are meant to be 1D list, and the pixels 2D
+	labels = []
+	pixels = []
+
+	# read in the lables and images info
+	with open(path, 'r') as fil:
+		r = csv.reader(fil)
+		# preprocessing, go through every pixel of every image
+		for i, line in enumerate(r):
+			lbl = (int(line[0]))
+			pxl = line[1:]
+			# insert the bias at the beginning of the list
+			pxl.insert(0, 1.0)
+
+			# divide every pixel value to be between 0 and 1
+			for j in range(len(pxl)):
+				pxl[j] = float(pxl[j])
+				pxl[j] = pxl[j]/255
+
+			# store the values to be returned
+			labels.append(lbl)
+			pixels.append(pxl)
+
+	return labels, pixels
