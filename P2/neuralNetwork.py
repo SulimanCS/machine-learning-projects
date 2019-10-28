58fcabf0dae1b3b07fbc47698033b772902e8e7e
Suliman Alsarraf <suliman2@pdx.edu>
2019-10-28 15:26:44 -0700

    Added the information header

--- P2/neuralNetwork.py
+++ P2/neuralNetwork.py
@@ -1,3 +1,9 @@
+# ==============================================
+# Name: Suliman Alsarraf
+# Assignment: Project 2
+# Class: CS445 Machine Learning
+# ==============================================
+
 import sys
 import os
 import csv
