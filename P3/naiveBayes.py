eef0485ba7618a947779a80c3bbd81366e985f90
Suliman Alsarraf <suliman2@pdx.edu>
2019-11-09 19:08:36 -0800

    Created naive bayes class

--- P3/naiveBayes.py
+++ P3/naiveBayes.py
@@ -1,3 +1,20 @@
 import sys
 import numpy as np
 
+# the naiveBayes class houses
+# the naive bayes structure and
+# specifications
+class naiveBayes:
+
+	numClasses = None
+	uniqueClasses = None
+
+	# when constructing the class,
+	# assign the number of unique classes
+	# that the training set has
+	def __init__(self, numClasses, classes):
+		self.numClasses = numClasses
+		self.uniqueClasses = {}
+		for i in classes:
+			self.uniqueClasses[int(i)] = {}
+
