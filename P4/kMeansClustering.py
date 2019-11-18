0700ea437f3aaba1b7815381dd2b09c03965f449
Suliman Alsarraf <suliman2@pdx.edu>
2019-11-17 23:10:48 -0800

    Created k means clustering class

--- P4/kMeansClustering.py
+++ P4/kMeansClustering.py
@@ -1,3 +1,14 @@
 import numpy as np
 import sys
 
+# the kMeansClustering class houses
+# the K-Means CLustering structure
+# and specifications
+class kMeansClustering:
+
+	k = None
+
+	# when constructing the class,
+	# assign the number of clusters K
+	def __init__(self, k): self.k = k
+
