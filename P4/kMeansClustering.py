125d5446f24c7817e97c3d7bbca9e48829f06214
Suliman Alsarraf <suliman2@pdx.edu>
2019-11-17 23:12:33 -0800

    Created loadSet function that loads a given dataset into a numpy array

--- P4/kMeansClustering.py
+++ P4/kMeansClustering.py
@@ -12,3 +12,10 @@ class kMeansClustering:
 	# assign the number of clusters K
 	def __init__(self, k): self.k = k
 
+# loads the dataset file elements as floats into a 2D numpy array
+def loadSet(filename):
+
+	# for our experiment, all the datasets attributes
+	# are separated by a comma
+	return np.loadtxt(filename, delimiter=',', dtype=np.float64)
+
