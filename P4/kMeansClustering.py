c51fdc225becbcdc8f7dd544638445b1e7e37087
Suliman Alsarraf <suliman2@pdx.edu>
2019-11-25 08:39:11 -0800

    General code cleanup

--- P4/kMeansClustering.py
+++ P4/kMeansClustering.py
@@ -44,11 +44,6 @@ class kMeansClustering:
 
 		numRuns = 5
 
-		# rows = [[0,1], [1,0], [2,0], [4,0]]
-		# samples = [[1,1], [4,1]]
-		# rows = np.asarray(rows, dtype=np.float64)
-		# samples = np.asarray(samples, dtype=np.float64)
-
 		# out of the N runs, store the necessary data for the
 		# best one
 		bestRun = None
@@ -92,10 +87,7 @@ class kMeansClustering:
 				# save the row number in a different variable
 				for rowNum, row in enumerate(rows):
 					clusterResults = {key: [] for key in range(len(samples))}
-					# print(row, '---', clusterResults)
 					for sampleNum, sample in enumerate(samples):
-						# print(sampleNum)
-						# print(sample)
 						res = 0
 						for index in range(len(row)):
 							res += np.square(np.sqrt(np.square(row[index] - sample[index])))
@@ -107,9 +99,6 @@ class kMeansClustering:
 				for clusterCenter in clusterMembership:
 					if clusterMembership[clusterCenter] == []:
 						samples[clusterCenter] = np.zeros(len(samples[0]), dtype=np.float64)
-						# TODO change this after trying k=30
-						print('yes')
-						exit(1)
 					else:
 						clusterRows = rows[clusterMembership[clusterCenter]]
 						for i in range(len(rows[0])):
@@ -122,25 +111,16 @@ class kMeansClustering:
 						'clusterMembership': copy.deepcopy(clusterMembership)}
 					else:
 						if bestRun['AMSE'] > AMSE:
-							print('YES')
 							bestRun = {'AMSE': AMSE, 'clusterCenters': copy.deepcopy(samples), \
 							'clusterMembership': copy.deepcopy(clusterMembership)}
-							# exit(1)
 
 					print('exiting, ', runNum)
-					# exit(1)
 					break
 				if oldClusterMembership == None or oldClusterMembership != clusterMembership:
-					# print('we are here')
 					oldClusterMembership = copy.deepcopy(clusterMembership)
 
-					# exit(1)
-				# print('final samples:', samples)
-				# print('final cl:', clusterMembership)
 				tryy+=1
-				# if tryy == 2:
-				# 	exit(1)
-				print('try:', tryy, '  run num:', runNum)
+				# print('try:', tryy, '  run num:', runNum)
 
 		bestRun['MSS'] = self.meanSquareSeparation(bestRun['clusterCenters'])
 		bestRun['meanEntropy'] = self.meanEntropy(trainSet, bestRun['clusterMembership'])
@@ -152,7 +132,6 @@ class kMeansClustering:
 
 	def averageMeanSquareError(self, data, clusterCenters, clusterMembership):
 
-		# print('in averageMeanSquareError')
 		averageMSE = []
 		for i in clusterMembership:
 			res=0
@@ -162,41 +141,28 @@ class kMeansClustering:
 					euclideanDistance = np.square(np.sqrt(np.square(data[entry, attributeNum] - clusterCenters[i, attributeNum])))
 					finalEuclideanDistance+=euclideanDistance
 				res+=finalEuclideanDistance
-				# print(finalEuclideanDistance)
 			averageMSE.append(res/len(clusterMembership[i]))
 
 		return sum(averageMSE)/len(clusterMembership)
 
 	def meanSquareSeparation(self, clusterCenters):
-		# print('MSS')
+
 		denamonitor = (len(clusterCenters)*(len(clusterCenters)-1))/2
-		# print('m8am', denamonitor)
 		res=0
 		for i in range(len(clusterCenters)):
-			# print('pair i', i, 'with')
 			for j in range(i+1, len(clusterCenters)):
-				# print(j)
 				for z in range(len(clusterCenters[0])):
 					euclideanDistance = np.square(np.sqrt(np.square(clusterCenters[i, z] - clusterCenters[j, z])))
 					res+=euclideanDistance
 		res = res/denamonitor
-		# print('final: ', res)
 		return res
 
 	def meanEntropy(self, trainSet, clusterMembership):
 
-		# print(trainSet)
-		# print(len(trainSet))
-		# print(len(trainSet[0]))
-		# exit(1)
-		print('in mean entropy')
 		entropy = {key: 0 for key in clusterMembership}
 		for cluster in clusterMembership:
-			print('---')
 			clusterDistribution = {}
 			total=0
-			# print(clusterMembership[cluster])
-			# print(cluster)
 			for index in clusterMembership[cluster]:
 				lastColumn = len(trainSet[index]) - 1
 				if trainSet[index, lastColumn] not in clusterDistribution:
@@ -205,42 +171,24 @@ class kMeansClustering:
 					clusterDistribution[trainSet[index, lastColumn]]+=1
 
 				total+=1
-			print(clusterDistribution)
-			# print('new start')
 			res=0
 			for i in clusterDistribution:
-				# print(clusterDistribution[i])
-				# print('final', clusterDistribution[i]/total * np.log2(clusterDistribution[i]/total))
 				res+=clusterDistribution[i]/total * np.log2(clusterDistribution[i]/total)
 			res = res * -1
 			entropy[cluster] = res
-		# 	print('new finish')
-		# 	print('---')
-		# print('entropy final', entropy)
-		# print('CM', clusterMembership)
-		# print('len of trainset', len(trainSet))
-		# fires = 0
-		# for c in clusterMembership:
-		# 	fires+=len(clusterMembership[c])
-		# print('fires', fires)
-		# TODO see the slides, calculate MEAN entropy
-		# TODO almost done
+
 		meanEntropy = 0
 		for cluster in clusterMembership:
 			meanEntropy += (len(clusterMembership[cluster])/len(trainSet)) \
 			* entropy[cluster]
-		print(meanEntropy)
 		return meanEntropy
 
 	def classify(self, trainSet, testSet, clusterMembership, clusterCenters):
 
 		# associate each cluster center with the most frequent class it contains
 		frequentClassPerCluster = {}
-		# print(len(trainSet))
-		# print(len(trainSet[0]))
 		lastIndex = len(trainSet[0]) - 1
 		for cluster in clusterMembership:
-			# print(clusterMembership[cluster])
 			clusterLabels = {}
 			for index in clusterMembership[cluster]:
 				classs = trainSet[index, lastIndex]
@@ -249,13 +197,7 @@ class kMeansClustering:
 				else:
 					clusterLabels[classs]+=1
 
-				# print(classs)
-			# print(clusterLabels)
-			# print(max(clusterLabels, key=clusterLabels.get))
 			frequentClassPerCluster[cluster] = int(max(clusterLabels, key=clusterLabels.get))
-			# exit(1)
-		# print('final')
-		# print(frequentClassPerCluster)
 
 		# assign each test instance the class of the closest cluster center
 		# compute accuracy and confusion matrix
@@ -278,20 +220,12 @@ class kMeansClustering:
 		# save the row number in a different variable
 		for rowNum, row in enumerate(rows):
 			clusterResults = {key: [] for key in range(len(clusterCenters))}
-			# print(row, '---', clusterResults)
 			for sampleNum, sample in enumerate(clusterCenters):
-				# print(sampleNum)
-				# print(sample)
 				res = 0
 				for index in range(len(row)):
 					res += np.square(np.sqrt(np.square(row[index] - sample[index])))
 				clusterResults[sampleNum] = res
-			# print('cluster results', clusterResults)
 			minDistance = min(clusterResults, key=clusterResults.get)
-			# print('min distance', minDistance)
-			# print('rowNum', rowNum)
-			# print('row label', labels[rowNum])
-			# print('FCPC minDis', frequentClassPerCluster[minDistance])
 			if frequentClassPerCluster[minDistance] == labels[rowNum]:
 				correct+=1
 			confusionMatrix[int(labels[rowNum])][int(frequentClassPerCluster[minDistance])]+=1
@@ -299,10 +233,7 @@ class kMeansClustering:
 		print(pd.DataFrame(confusionMatrix))
 		print('accuracy=', correct/total)
 
-		# print(len(clusterCenters))
 		for clusterNum, centerPoint in enumerate(clusterCenters):
-			# print('Center PP', centerPoint)
-			# print('type Center PP', type(centerPoint))
 			imagePixels = copy.deepcopy(centerPoint)
 			imagePixels = imagePixels.reshape(8,8)
 			plt.imshow(imagePixels, cmap="gray")
@@ -310,7 +241,6 @@ class kMeansClustering:
 			filename = 'k'+str(self.k)+'-'+str(clusterNum)+'.png'
 			plt.savefig(filename)
 
-			# exit(1)
 		writeConfusionMatrixToCSV(self.k, confusionMatrix)
 
 def writeConfusionMatrixToCSV(k, confusionMatrix):
