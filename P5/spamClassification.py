4e7019d11657feb8fb2e383e84e72503aa6a12bc
Suliman Alsarraf <suliman2@pdx.edu>
2019-11-30 22:22:19 -0800

    Created preprocessing function where it sets up the data (split train, test, train labels, test labels) before the classification process

--- P5/spamClassification.py
+++ P5/spamClassification.py
@@ -12,3 +12,54 @@ def loadSet(filename):
 	# are separated by a comma
 	return np.loadtxt(filename, delimiter=',', dtype=np.float64)
 
+def preprocessing(data):
+
+	# setup the data
+
+	# total length of dataset
+	totalLen = len(data)
+
+	# index of last element
+	lastElement = len(data[0]) - 1
+
+	# get all indices where row is not a spam (0)
+	zeroesIndices = np.where(data[:, lastElement] == 0)[0]
+
+	# get all indices where row is a spam (1)
+	onesIndices = np.where(data[:, lastElement] == 1)[0]
+
+	# store all non spam rows in a separate numpy array
+	zeroes = copy.deepcopy(data[zeroesIndices, :])
+
+	# store all spam rows in a separate numpy array
+	ones = copy.deepcopy(data[onesIndices, :])
+
+	# split train and test set 50% 50%
+	# with equal amount of 0s and 1s
+
+	# initialize train set
+	trainSet = np.empty((0, len(data[0])), dtype=np.float64)
+
+	# stack half of 0s and 1s into the train set
+	trainSet = np.vstack((trainSet, zeroes[0:len(zeroes)//2, :]))
+	trainSet = np.vstack((trainSet, ones[0:len(ones)//2, :]))
+
+	# initialize test set
+	testSet = np.empty((0, len(data[0])), dtype=np.float64)
+
+	# stack half of 0s and 1s into the test set
+	testSet = np.vstack((testSet, zeroes[len(zeroes)//2:len(zeroes), :]))
+	testSet = np.vstack((testSet, ones[len(ones)//2:len(ones), :]))
+
+	# extract labels for both sets
+	trainLabels = trainSet[:, -1]
+	testLabels = testSet[:, -1]
+
+	# both sets ignore the class column
+	trainSet= trainSet[:, :-1]
+	testSet= testSet[:, :-1]
+
+	# total length of train and test set should
+	# be equal to total dataset length
+	assert(len(trainSet) + len(testSet) == len(data))
+
