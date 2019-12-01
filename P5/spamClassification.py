fdd35f638c53172d989af3b855ae4be9f3ac286e
Suliman Alsarraf <suliman2@pdx.edu>
2019-11-30 22:01:37 -0800

    Created loadSet function that loads a given dataset into a numpy array

--- P5/spamClassification.py
+++ P5/spamClassification.py
@@ -5,3 +5,10 @@ from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
 import copy
 
+# loads the dataset file elements as floats into a 2D numpy array
+def loadSet(filename):
+
+	# for our experiment, all the datasets attributes
+	# are separated by a comma
+	return np.loadtxt(filename, delimiter=',', dtype=np.float64)
+
