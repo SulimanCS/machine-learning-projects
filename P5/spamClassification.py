b3c07f6a4ece37f9723da9361798990a86e74add
Suliman Alsarraf <suliman2@pdx.edu>
2019-11-30 22:01:06 -0800

    Created spamClassification.py

---
+++ P5/spamClassification.py
@@ -0,0 +1,7 @@
+import sklearn
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+import copy
+
